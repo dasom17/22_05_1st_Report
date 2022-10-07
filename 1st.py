db0897a9fd6f4b5d71bf60acf9eb46ab2e37be6a
dasom17 <112676952+dasom17@users.noreply.github.com>
2022-10-07 20:54:23 +0900

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+t = turtle.Turtle()
+t.shape("turtle");
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
